161cd7664afcf588bdf5ef24a341955adee8aa74
Howard Y <howardyaohs@gmail.com>
2020-08-21 04:22:04 +0000

    m

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-console.log('Pythontutorials(pytutorials) version  Alpha 1.1.0')
+
+
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 const js = async () => {
-  
+console.log('Pythontutorials (pytutorials) version Alpha 1.1.2')
+await sleep(500)
+console.log('Welcome!') 
 }
 
 js()
