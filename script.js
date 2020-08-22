c29deecf446d6b7260b1686fd48741065dcb615b
Howard Y <howardyaohs@gmail.com>
2020-08-22 01:44:37 +0000

    something

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-console.log('Pythontutorials (pytutorials) Alpha 1.0.9')
-console.log('Note: The site-versions tab are not available anymore. Those who are still using Alpha 1.1.2 or 1.1.0, there will be a forced update to Alpha 1.0.9.')
+console.log('Pythontutorials (pytutorials) Alpha 1.1.9')
+console.log()
+console.log('Note: The site-versions tab are not available anymore. Those who are still using Alpha 1.1.2 or 1.1.0, there will be a forced update to Alpha 1.1.9.')
